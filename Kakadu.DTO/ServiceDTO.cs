d3beb154b8a35da9513f064bd17b7f930477497a
Rafal <rafal@szumilas.org>
2020-01-28 00:12:34 +0100

    adding servicedto

---
+++ Kakadu.DTO/ServiceDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Kakadu.DTO
+{
+    public class ServiceDTO
+    {
+        public Guid Id { get; set; }
+        
+        public string Code { get; set; }
+
+        public string Name { get; set; }
+
+        public Uri Address { get; set; }
+
+        public bool UnkownRoutesPassthrough { get; set; }
+    }
+}
