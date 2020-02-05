2e46f3ff9399533e86a08cf693c928f1b96ea060
Rafal <rafal@szumilas.org>
2020-02-05 18:55:03 +0100

    instances constant

--- Kakadu.DTO/Constants/KakaduConstants.cs
+++ Kakadu.DTO/Constants/KakaduConstants.cs
@@ -3,5 +3,6 @@ namespace Kakadu.DTO.Constants
     public static class KakaduConstants
     {
         public const string ACCESS_TOKEN = "KakaduApiAccessToken";
+        public const string ACTIONAPI_INSTANCES = "KakaduActionApiInstances";
     }
 }
