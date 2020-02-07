cd9297a3dd81c6f408ef148f9edf8947f4d5fbbc
Rafal <rafal@szumilas.org>
2020-02-07 10:08:29 +0100

    httprefenceexcetion

--- Kakadu.ConfigurationApi/Startup.cs
+++ Kakadu.ConfigurationApi/Startup.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using AutoMapper;
-using Kakadu.ConfigurationApi.Middleware;
+using Kakadu.Common.Middleware;
 using Kakadu.ConfigurationApi.Settings;
 using Kakadu.Core;
 using LazyCache;
