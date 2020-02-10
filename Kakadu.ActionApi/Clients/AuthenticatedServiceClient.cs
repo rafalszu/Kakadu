18a58046447c8b766dbadd242b0a779ba2577aec
Rafal <rafal@szumilas.org>
2020-02-10 14:23:43 +0100

    store reply signature update

--- Kakadu.ActionApi/Clients/AuthenticatedServiceClient.cs
+++ Kakadu.ActionApi/Clients/AuthenticatedServiceClient.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
@@ -12,14 +13,22 @@ namespace Kakadu.ActionApi.Clients
 {
     public class AuthenticatedServiceClient : AnonymousServiceClient, IAuthenticatedServiceClient
     {
+        private readonly ILogger<AuthenticatedServiceClient> _logger;
+
         public AuthenticatedServiceClient(HttpClient client, ILogger<AuthenticatedServiceClient> logger, IAppCache cache) : base(client, logger, cache)
         {
+            _logger = logger;
+
             string accessToken = cache.Get<string>(KakaduConstants.ACCESS_TOKEN);
             client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", accessToken);
         }
 
-        public async Task StoreReply()
+        public async Task StoreReply(KnownRouteDTO dto)
         {
+            if(dto == null)
+                throw new ArgumentNullException(nameof(dto));
+                
+            _logger.LogInformation("dto sent to configuration api");
             // TODO: send recorded replies to config api
         }
     }
--- Kakadu.ActionApi/Interfaces/IAuthenticatedServiceClient.cs
+++ Kakadu.ActionApi/Interfaces/IAuthenticatedServiceClient.cs
@@ -1,10 +1,11 @@
 using System.Threading.Tasks;
+using Kakadu.DTO;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Kakadu.ActionApi.Interfaces
 {
     public interface IAuthenticatedServiceClient : IAnonymousServiceClient
     {
-        Task StoreReply();
+        Task StoreReply(KnownRouteDTO dto);
     }
 }
