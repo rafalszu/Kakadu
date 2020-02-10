8b17d9c1ecc9e67dc61100d8bdf659603ad17e10
Rafal <rafal@szumilas.org>
2020-02-10 15:06:29 +0100

    start recording

--- Kakadu.ConfigurationApi/Controllers/v1/RecordController.cs
+++ Kakadu.ConfigurationApi/Controllers/v1/RecordController.cs
@@ -2,12 +2,15 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
+using System.Threading;
 using System.Threading.Tasks;
+using Kakadu.ConfigurationApi.Interfaces;
 using Kakadu.DTO.Constants;
 using Kakadu.DTO.HttpExceptions;
 using LazyCache;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 
 namespace Kakadu.ConfigurationApi.Controllers.v1
 {
@@ -18,35 +21,66 @@ namespace Kakadu.ConfigurationApi.Controllers.v1
     public class RecordController : ControllerBase
     {
         private readonly IAppCache _cache;
+        private readonly IActionApiHttpClient _actionApiHttpClient;
+        private readonly ILogger<RecordController> _logger;
 
-        public RecordController(IAppCache cache)
+        public RecordController(IAppCache cache, IActionApiHttpClient actionApiHttpClient, ILogger<RecordController> logger)
         {
+            _logger = logger;
+            _actionApiHttpClient = actionApiHttpClient;
             _cache = cache;
         }
 
         [HttpPost("start/{serviceCode}")]
-        public async Task<ActionResult> StartRecording(string serviceCode)
+        public async Task<ActionResult> StartRecording(string serviceCode, CancellationToken cancellationToken)
         {
-            if(string.IsNullOrWhiteSpace(serviceCode))
+            if (string.IsNullOrWhiteSpace(serviceCode))
                 throw new ArgumentNullException(nameof(serviceCode));
 
             List<string> instances = await _cache.GetAsync<List<string>>(KakaduConstants.ACTIONAPI_INSTANCES);
-            if(instances == null || !instances.Any())
+            if (instances == null || !instances.Any())
                 throw new HttpResponseException(HttpStatusCode.UnprocessableEntity, "No Action API instances registered within Configuration API. Ensure at least Action API instance is running and can connect to Configuration API");
 
-            // TODO: send request to each action api to start reporting back routes
+            var tasks = Task.WhenAll(
+                instances.Select(instance => StartRecordingOnActionApi(instance, cancellationToken))
+            );
+
+            try
+            {
+                await tasks;
+            }
+            catch
+            {
+                // process exception
+                var x = tasks.Exception;
+            }
 
             return Ok();
+        }
+
+        private async Task<bool> StartRecordingOnActionApi(string instance, CancellationToken cancellationToken)
+        {
+            if (string.IsNullOrWhiteSpace(instance))
+                throw new ArgumentNullException(nameof(instance));
+
+            try
+            {
+                return await _actionApiHttpClient.StartRecording(instance, cancellationToken);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"Unable to start recording on node '{instance}': {ex.Message}");
+            }
         }
 
         [HttpPost("stop/{serviceCode}")]
         public async Task<ActionResult> StopRecording(string serviceCode)
         {
-            if(string.IsNullOrWhiteSpace(serviceCode))
+            if (string.IsNullOrWhiteSpace(serviceCode))
                 throw new ArgumentNullException(nameof(serviceCode));
 
             List<string> instances = await _cache.GetAsync<List<string>>(KakaduConstants.ACTIONAPI_INSTANCES);
-            if(instances == null || !instances.Any())
+            if (instances == null || !instances.Any())
                 throw new HttpResponseException(HttpStatusCode.UnprocessableEntity, "No Action API instances registered within Configuration API. Ensure at least Action API instance is running and can connect to Configuration API");
 
             // TODO: send request to each action api to stop reporting back routes
