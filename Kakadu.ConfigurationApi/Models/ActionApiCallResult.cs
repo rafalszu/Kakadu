0eb88b187a0b27f1bfb3b3704d369ecaf6dc51e5
Rafal <rafal@szumilas.org>
2020-02-14 16:07:08 +0100

    adding actionapicallresult

---
+++ Kakadu.ConfigurationApi/Models/ActionApiCallResult.cs
@@ -0,0 +1,9 @@
+namespace Kakadu.ConfigurationApi.Models
+{
+    public class ActionApiCallResult
+    {
+        public string Host { get; set; }
+
+        public object Result { get; set; }
+    }
+}
